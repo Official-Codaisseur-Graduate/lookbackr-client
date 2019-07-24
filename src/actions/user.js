73134007dbda875bd486781aa00ae90a81b35abb
Fraxcelsior <marten_bonnema@hotmail.com>
2019-07-24 13:44:08 +0200

    add basic action for updating user

--- src/actions/user.js
+++ src/actions/user.js
@@ -17,3 +17,28 @@ export const succesUser = (username) => (dispatch) => {
     })
     .catch(console.error)
 }
+/*
+export const USER_UPDATE = 'USER_UPDATE'
+
+const updateSucces = (update) => ({
+  type: USER_UPDATE,
+  payload: update
+})*/
+
+/** This action should receive a user Id and room/retro Id 
+ * It updates the specified user's retroId with the specifeid retro Id
+*/
+export const updateUser = (userId, roomId) => (dispatch) => {
+  request
+    .put(`${baseUrl}/rooms/${roomId}`)
+    .send(
+      {user:{id: userId}}
+    )
+    .then(response => {
+      console.log('UPDATED USER:', response.body)
+      dispatch(userSucces(response.body))
+    })
+    .catch(console.error)
+}
+
+//=> (dispatch)
