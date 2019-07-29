d4494edcfcb604bcb42d856235e6ecae9e90d172
Astrid88 <giu.munari@gmail.com>
2019-07-29 13:16:15 +0200

    changes to fix typos

--- src/components/Retro/RetroContainer.js
+++ src/components/Retro/RetroContainer.js
@@ -8,90 +8,102 @@ import CardForm from './CardForm';
 import { Link } from 'react-router-dom'
 
 class RetroContainer extends Component {
-  id = this.props.match.params.id
+    id = this.props.match.params.id
 
-  state = {
-    type: '',
-    text: '',
-    visibilityForm: false,
-    message_submit: 'share your cards'
-  }
-    
-  componentDidMount() {
-    const userId = this.props.currentUser.id
-    this.props.updateUser(userId, parseInt(this.id))
-  }
-  
-  onChange = (event) => {
-    this.setState({
-    [event.target.name]: event.target.value
-    })
-  }
-    
-  onSubmit = (event) => {
-    event.preventDefault()
-    const userId = this.props.currentUser.id
-    this.props.addCardInState(this.state, userId, this.id, this.props.userCards)
-    this.setState({
-      type: '',
-      text: ''
-    })
-  }
-    
-  submitChanges = () => {
-    this.props.userDone(this.props.currentUser.id, parseInt(this.id))
-    this.setState({
-      message_submit: 'waiting for the next user...'
-    })
-  }
-    
-  toggleVisibility = () => {
-    if (this.state.visibilityForm === true) {
-      return this.setState({ visibilityForm: false })
-    } else {
-      return this.setState({ visibilityForm: true })
+    state = {
+        type: '',
+        text: '',
+        visibilityForm: false,
+        message_submit: 'share your cards'
     }
-  }
-    
-  joinedUser = 'Joining room...'
-  optionsCards = ['mad', 'sad', 'glad']
-  optionsCard2 = ['stop', 'start', 'keep']
-  
-  render() {
-    return (
-      <div className='container'>
-        {!this.props.retro && <Loader/>}
-        {(this.props.retro && this.props.users) &&
-          <div>
-            <div className='description'>
-              <p>{this.props.retro.description}</p>
-              {this.props.users.map(user => user.username + ', ')}
-              {this.joinedUser}
+
+    componentDidMount() {
+        const userId = this.props.currentUser.id
+        this.props.updateUser(userId, parseInt(this.id))
+
+    }
+    onChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault()
+        const userId = this.props.currentUser.id
+        this.props.addCardInState(this.state, userId, this.id, this.props.userCards)
+        this.setState({
+            type: '',
+            text: ''
+        })
+    }
+    submitChanges = () => {
+        this.props.userDone(this.props.currentUser.id, parseInt(this.id))
+        this.setState({
+            message_submit: 'waiting for the next user...'
+        })
+    }
+
+    toggleVisibility = () => {
+        if (this.state.visibilityForm === true) {
+            return this.setState({
+                visibilityForm: false
+            })
+        }
+        else {
+            return this.setState({
+                visibilityForm: true
+            })
+        }
+    }
+
+    joinedUser = 'Joining room...'
+    optionsCards = ['mad', 'sad', 'glad']
+    optionsCard2 = ['stop', 'start', 'keep']
+
+    render() {
+        return (
+            <div className='container'>
+                {!this.props.retro &&
+                    <Loader />
+                }
+                {(this.props.retro && this.props.users) &&
+                    <div>
+                        <div className='description'>
+                            <p>{this.props.retro.description}</p>
+                            {this.props.users.map(user => user.username + ', ')}
+                            {this.joinedUser}
+                        </div>
+                    </div>
+                }
+                {this.props.cards && <Retro cards={this.props.cards} users={this.props.users}/>}
+
+                {!this.props.done &&
+                    <div>
+                        <button className='button' onClick={this.submitChanges}>{this.state.message_submit}</button>
+
+                        <div id='createCardFormContainer'>
+                            <CardForm
+                                onSubmit={this.onSubmit}
+                                onChange={this.onChange}
+                                values={this.state}
+                                options={this.optionsCards}
+                                visibilityForm={this.state.visibilityForm}
+                                toggleVisibility={this.toggleVisibility}
+                            />
+                        </div>
+                    </div>
+                }
+
+                <Link to={`/retrospectives/${this.id}/next`} className='button next'>go to the next fase</Link>
+                {this.props.done &&
+                    <div>
+                         
+                    </div>
+                }
             </div>
-          </div>
-        }
-        {this.props.cards && <Retro cards={this.props.cards} users={this.props.users}/>}
-        {!this.props.done &&
-          <div>
-            <button className='button' onClick={this.submitChanges}>{this.state.message_submit}</button>
-            <div id='createCardFormContainer'>
-              <CardForm
-                onSubmit={this.onSubmit}
-                onChange={this.onChange}
-                values={this.state}
-                options={this.optionsCards}
-                visibilityForm={this.state.visibilityForm}
-                toggleVisibility={this.toggleVisibility}
-              />
-            </div>
-          </div>
-        }
-        {this.props.done &&
-          <Link to={`/retrospectives/${this.id}/next`} className='button next'>go to the next fase</Link>
-        }
-      </div>
-    )
-  }
+        )
+    }
 }
 
 const mapStateToProps = (state, ownProps) => {
@@ -99,6 +111,7 @@ const mapStateToProps = (state, ownProps) => {
     const retro = state.lobby.find(retro => retro.id === retroId)
     const userCards = retro.cards.filter(card => card.userId === state.user.id)
     const cards = retro.done ? retro.cards : userCards
+
     
     return {
         done: retro.done,
@@ -108,4 +121,5 @@ const mapStateToProps = (state, ownProps) => {
         currentUser: state.user
     }
 }
+
 export default connect(mapStateToProps, { addCardInState, updateUser, userDone })(RetroContainer) 
--- src/components/Retro/RetroNextContainer.js
+++ src/components/Retro/RetroNextContainer.js
@@ -9,93 +9,107 @@ import CardForm from './CardForm';
 import { Link } from 'react-router-dom'
 
 class RetroNextContainer extends Component {
-  id = this.props.match.params.id
-  
-  state = {
-    type: '',
-    text: '',
-    visibilityForm: false,
-    message_submit: 'share your cards'
-  }
-  
-  componentDidMount() {
-    this.props.userRestart(this.props.currentUser.id, parseInt(this.id))
-  }
-    
-  onChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value })
-  }
-    
-  onSubmit = (event) => {
-    event.preventDefault()
-    const userId = this.props.currentUser.id
-    this.props.addCardInState(this.state, userId, this.id, this.props.userCards)
-    this.setState({
-      type: '',
-      text: ''
-    })
-  }
-    
-  submitChanges = () => {
-    this.props.userDone(this.props.currentUser.id, parseInt(this.id))
-    this.setState({ message_submit: 'waiting for the next user...' })
-  }
+    id = this.props.match.params.id
 
-  toggleVisibility = () => {
-    if (this.state.visibilityForm === true) {
-      return this.setState({ visibilityForm: false })
-    } else {
-      return this.setState({ visibilityForm: true })
+    state = {
+        type: '',
+        text: '',
+        visibilityForm: false,
+        message_submit: 'share your cards'
     }
-  }
-    
-  optionsCards = ['mad', 'sad', 'glad']
-  optionsCard2 = ['stop', 'start', 'keep']
-  
-  render() {
-    return (
-      <div className='container'>
-        {!this.props.retro && <Loader /> }
-        {(this.props.retro && this.props.users) &&
-          <div>
-            <div className='description'>
-              <p>{this.props.retro.description}</p>
-              {this.props.users.map(user => user.username + ' ')}
-            </div>
-          </div>
-        }
-        {this.props.cards &&
-          <div>
-            <Link to={`/repositories`} className='button next'>Go to the Homepage</Link>
-            <Retro cards={this.props.retro.cards} users={this.props.users} />
-            <Retro2 cards={this.props.cards} users={this.props.users} />
-          </div>
-        }
-        {!this.props.done &&
-          <div>
-            <button className='button' onClick={this.submitChanges}>{this.state.message_submit}</button>                  
-              <div id='createCardFormContainer'>
-                <CardForm
-                  onSubmit={this.onSubmit}
-                  onChange={this.onChange}
-                  values={this.state}
-                  options={this.optionsCard2}
-                  visibilityForm={this.state.visibilityForm}
-                  toggleVisibility={this.toggleVisibility}
-                />
-              </div>
-          </div>
-        }
-      </div>
-    )
-  }
-}
+    componentDidMount() {
+        this.props.userRestart(this.props.currentUser.id, parseInt(this.id))
+    }
+    onChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    onSubmit = (event) => {
+        event.preventDefault()
+        const userId = this.props.currentUser.id
+        this.props.addCardInState(this.state, userId, this.id, this.props.userCards)
+        this.setState({
+            type: '',
+            text: ''
+        })
+    }
 
+    submitChanges = () => {
+        this.props.userDone(this.props.currentUser.id, parseInt(this.id))
+        this.setState({
+            message_submit: 'waiting for the next user...'
+        })
+    }
+
+    toggleVisibility = () => {
+        if (this.state.visibilityForm === true) {
+            return this.setState({
+                visibilityForm: false
+            })
+        }
+        else {
+            return this.setState({
+                visibilityForm: true
+            })
+        }
+    }
+
+    optionsCards = ['mad', 'sad', 'glad']
+    optionsCard2 = ['stop', 'start', 'keep']
+
+    render() {
+
+        return (
+            <div className='container'>
+                {!this.props.retro &&
+                    <Loader />
+                }
+                {(this.props.retro && this.props.users) &&
+                    <div>
+                        <div className='description'>
+                            <p>{this.props.retro.description}</p>
+                            {this.props.users.map(user => user.username + ' ')}
+                        </div>
+
+                    </div>
+                }
+                {this.props.cards &&
+                    <div>
+                        <Link to={`/repositories`} className='button next'>Go to the Homepage</Link>
+                        <Retro cards={this.props.retro.cards} users={this.props.users} />
+                        <Retro2 cards={this.props.cards} users={this.props.users} />
+                    </div>
+                }
+                {!this.props.done &&
+                    <div>
+                        <button className='button' onClick={this.submitChanges}>{this.state.message_submit}</button>                  
+                        <div id='createCardFormContainer'>
+                            <CardForm
+                                onSubmit={this.onSubmit}
+                                onChange={this.onChange}
+                                values={this.state}
+                                options={this.optionsCard2}
+                                visibilityForm={this.state.visibilityForm}
+                                toggleVisibility={this.toggleVisibility}
+                            />
+                        </div>
+                    </div>
+                }
+
+                <div>
+                </div>
+
+            </div>
+        )
+    }
+}
 function mapStateToProps(state, ownProps) {
     const retroId = parseFloat(ownProps.match.params.id)
     const retro = state.lobby.find(retro => retro.id === retroId)
     const userCards = retro.cards.filter(card => card.userId === state.user.id)
     const cards = retro.done ? retro.cards : userCards
+
     return {
         done: retro.done,
         retro,
@@ -104,4 +118,5 @@ function mapStateToProps(state, ownProps) {
         currentUser: state.user
     }
 }
+
 export default connect(mapStateToProps, { addCardInState, userDone, userRestart })(RetroNextContainer)
