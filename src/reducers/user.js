f7a50723dbe49c0e5570852f98779c96739596c7
cynthiameiring <cynthia.meiring@hotmail.com>
2019-12-04 17:13:44 +0100

    Signup form

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,12 +1,14 @@
-import { USER_SUCCES} from '../actions/user'
-  
-const reducer = (state = {}, action) => {
-  switch(action.type) {
-    case USER_SUCCES:
-      return action.payload
-    default: 
-      return state
-  }
-}
+import { SIGNEDUP, USER_SUCCES } from "../actions/user";
 
-export default reducer
+const reducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_SUCCES:
+      return action.payload;
+    case SIGNEDUP:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
