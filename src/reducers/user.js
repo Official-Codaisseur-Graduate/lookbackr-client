871e29ca35e53a077db34c9137dc492e2a9c22fb
ahaigh9877 <alastairhaigh@me.com>
2019-12-05 15:01:40 +0100

    Remove console.log

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,7 +1,6 @@
 import { SIGNEDUP, AUTHENTICATION_JWT, LOGGEDOUT } from "../actions/user";
 
 const reducer = (state = {}, action) => {
-  console.log("A C T I O N ! ! ", action);
   switch (action.type) {
     // case USER_SUCCES:
     //   return action.payload;
