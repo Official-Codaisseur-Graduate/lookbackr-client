52c333c5391d8555e6357bccde886f41a39d7a82
cynthiameiring <cynthia.meiring@hotmail.com>
2019-12-05 12:18:21 +0100

    keep track of retroId for the current user

--- src/actions/user.js
+++ src/actions/user.js
@@ -44,31 +44,34 @@ export const login = (username, userpw) => dispatch => {
       console.log("error", res);
     });
 };
-export const USER_SUCCES = "USER_SUCCES";
+// export const USER_SUCCES = "USER_SUCCES";
 
-const userSucces = event => ({
-  type: USER_SUCCES,
-  payload: event
-});
+// const userSucces = event => ({
+//   type: USER_SUCCES,
+//   payload: event
+// });
 
-export const succesUser = username => dispatch => {
-  request
-    .post(`${baseUrl}/users`)
-    .send({ username })
-    .then(response => {
-      console.log("RESPONSE USER:", response.body);
-      dispatch(userSucces(response.body));
-    })
-    .catch(console.error);
-};
+// export const succesUser = username => dispatch => {
+//   request
+//     .post(`${baseUrl}/users`)
+//     .send({ username })
+//     .then(response => {
+//       console.log("RESPONSE USER:", response.body);
+//       dispatch(userSucces(response.body));
+//     })
+//     .catch(console.error);
+// };
 
-export const updateUser = (userId, roomId) => dispatch => {
+export const updateUser = (userId, roomId) => (dispatch, getState) => {
+  const state = getState();
+  const { user } = state;
   request
     .put(`${baseUrl}/enter-room/${roomId}`)
+    .set({ authorization: `Bearer ${user.jwt}` })
     .send({ user: { id: userId } })
-    .then(response => {
-      dispatch(userSucces(response.body));
-    })
+    // .then(response => {
+    //   dispatch(userSucces(response.body));
+    // })
     .catch(console.error);
 };
 
@@ -76,7 +79,7 @@ export const userDone = (userId, roomId) => dispatch => {
   request
     .put(`${baseUrl}/room/${roomId}`)
     .send({ user: { id: userId } })
-    .then(response => dispatch(userSucces(response.body)))
+    // .then(response => dispatch(userSucces(response.body)))
     .catch(console.error);
 };
 
@@ -86,7 +89,7 @@ export const userRestart = (userId, roomId) => dispatch => {
     .send({ user: { id: userId } })
     .then(response => {
       console.log("UPDATED USER DONE:", response.body);
-      dispatch(userSucces(response.body));
+      // dispatch(userSucces(response.body));
     })
     .catch(console.error);
 };
