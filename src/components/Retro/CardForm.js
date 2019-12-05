0004aa5c1945655a6a40fc326a152fd1b67a4662
ahaigh9877 <alastairhaigh@me.com>
2019-12-05 11:48:42 +0100

    Style "add card" form

--- src/components/Retro/CardForm.js
+++ src/components/Retro/CardForm.js
@@ -1,32 +1,67 @@
-import React from 'react'
+import React from "react";
 export default function CardForm(props) {
   return (
-    <div >
-
-      {props.visibilityForm &&
+    <div>
+      {props.visibilityForm && (
         <div>
-          <button onClick={props.toggleVisibility} className='button'>Hide the form</button>
-          <div className='form'>
-          <form id='createCard' onSubmit={props.onSubmit} onChange={props.onChange}>
-            <div className='form-lines'>
-            <select value={props.values.type} name='type' onChange={props.onChange} className='input' required>
-              <option value=""> Choose the type</option>
-              {props.options.map((option, index) => <option value={option} key={index} id={index}>{option}</option>)}
-            </select>
-            </div>
-            <div className='form-lines'>
-            <textarea type='text' name='text' value={props.values.text} onChange={props.onChange} placeholder='Why are you feeling this way?' className='input' required />
-            </div>
-            <div className='form-lines'>
-              <button type='submit' className='button'>Add</button>
-            </div>
-          </form>
+          <div className="form-container">
+            <form
+              id="createCard"
+              onSubmit={props.onSubmit}
+              onChange={props.onChange}
+            >
+              <br />
+              <div className="explanation-text">
+                <strong>Add a card!</strong>
+              </div>
+              {/* <div className="form-lines"> */}
+              <select
+                value={props.values.type}
+                name="type"
+                onChange={props.onChange}
+                className="input"
+                required
+              >
+                <option value=""> Choose the type</option>
+                {props.options.map((option, index) => (
+                  <option value={option} key={index} id={index}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+              {/* </div> */}
+              {/* <div className="form-lines"> */}
+              <textarea
+                type="text"
+                name="text"
+                value={props.values.text}
+                onChange={props.onChange}
+                placeholder="Why are you feeling this way?"
+                className="input"
+                required
+              />
+              {/* </div> */}
+              <div className="buttonsContainer">
+                <button type="submit" className="button">
+                  Add card
+                </button>
+                <button onClick={props.toggleVisibility} className="button">
+                  Hide form
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-      }
-      {!props.visibilityForm &&
-      <button onClick={props.toggleVisibility} className='button'>1 - Add cards</button>
-      }
+      )}
+      {!props.visibilityForm && (
+        <button
+          onClick={props.toggleVisibility}
+          className="button"
+          style={{ marginTop: "20px" }}
+        >
+          1 - Add cards
+        </button>
+      )}
     </div>
-  )
+  );
 }
