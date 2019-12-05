c7248aefd7b805f0ea33e0d09965a3a26b99f4c3
ahaigh9877 <alastairhaigh@me.com>
2019-12-05 09:27:59 +0100

    Correct a bit of the wording

--- src/components/Lobby/Lobby.js
+++ src/components/Lobby/Lobby.js
@@ -1,41 +1,44 @@
-import React from 'react'
-import LobbyForm from './LobbyForm'
-import { Link } from 'react-router-dom'
+import React from "react";
+import LobbyForm from "./LobbyForm";
+import { Link } from "react-router-dom";
 
 export default function Lobby(props) {
-  
-  const { onAdd, onChange, onSubmit, values, onClickDelete } = props
-  const { editMode } = values
-  const lobbyForm = <LobbyForm onChange={onChange} onSubmit={onSubmit} values={values} />
-  const form = editMode && lobbyForm
+  const { onAdd, onChange, onSubmit, values, onClickDelete } = props;
+  const { editMode } = values;
+  const lobbyForm = (
+    <LobbyForm onChange={onChange} onSubmit={onSubmit} values={values} />
+  );
+  const form = editMode && lobbyForm;
 
   return (
-      <div>
-        <div className='explanation-text'>
-          <p>Enter in a room with your team to start the retrospective meeting. Or create a new one.</p>
-          <button onClick={onAdd} className='button'>Make a new room</button>
-        </div>
-
-        {form}
     <div>
-      <div className='room-area'>
-      {props.lobby && props.lobby
-      .map((room, index) =>
-
-          <div className='room-item'  key={index} >
-            <Link to={`/retrospectives/${room.id}`}>{room.name}</Link>
-            <p>
-              {room.description}
-            </p>
-
-            <button className='buttonDelete' onClick={onClickDelete(room.id)}></button>
-          </div>
-        )}
+      <div className="explanation-text">
+        <p>
+          Enter a room with your team to start the retrospective meeting. Or
+          create a new one.
+        </p>
+        <button onClick={onAdd} className="button">
+          Make a new room
+        </button>
       </div>
 
+      {form}
+      <div>
+        <div className="room-area">
+          {props.lobby &&
+            props.lobby.map((room, index) => (
+              <div className="room-item" key={index}>
+                <Link to={`/retrospectives/${room.id}`}>{room.name}</Link>
+                <p>{room.description}</p>
 
-
+                <button
+                  className="buttonDelete"
+                  onClick={onClickDelete(room.id)}
+                ></button>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
-      </div>
-  )
+  );
 }
--- src/components/User/UserForm.js
+++ src/components/User/UserForm.js
@@ -1,24 +1,34 @@
-import React from 'react'
-import '../stylingComponents.css'
+import React from "react";
+import "../stylingComponents.css";
 
 export default function LoginForm(props) {
-  const { onChange, onSubmit, values } = props
-  
+  const { onChange, onSubmit, values } = props;
+
   return (
     <div>
-
-      <div className='text-home'>You’ve done it! You and your team completed an app together.
-          Of course you’re happy that the work is done.
-          But let’s take a few minutes and look back together.
-          Figure out what went well and what didn’t so you can improve and be even better the next project.</div>
-      <div className='form'>
-      <form onSubmit={onSubmit}>
-        <input name={'name'} onChange={onChange} value={values.name} placeholder='Fill your name' className='input' required/>
-        <div>
-          <button type='submit' className='button'>Enter</button>
-        </div>
-      </form>
+      <div className="text-home">
+        You’ve done it! You and your team completed an app together. Of course
+        you’re happy that the work is done. But let’s take a few minutes and
+        look back together. Figure out what went well and what didn’t so you can
+        improve and be even better on the next project.
+      </div>
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <input
+            name={"name"}
+            onChange={onChange}
+            value={values.name}
+            placeholder="Fill in your name"
+            className="input"
+            required
+          />
+          <div>
+            <button type="submit" className="button">
+              Enter
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
